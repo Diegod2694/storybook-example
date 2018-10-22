21f2eb75a9a81633b027f1c648b133302f1c83c8
Diego Parra <diegod2694@gmail.com>
2018-10-21 22:40:45 -0400

    Apply Style to a React StoryBook with the Info addon

--- src/Button.stories.js
+++ src/Button.stories.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { withInfo } from '@storybook/addon-info';
 
-import { storiesOf } from '@storybook/react'
-import { Button } from './Button'
+import { storiesOf } from '@storybook/react';
+import { Button } from './Button';
 
-storiesOf('Button', module).addWithJSX('with background', () => <Button bg="palegoldenrod">Hello World</Button>)
-storiesOf('Button', module).addWithJSX('with background green', () => <Button bg="green">Hello World</Button>)
+storiesOf('Button', module)
+  .addWithJSX(
+    'with background', 
+    withInfo({
+      styles: {
+        header: {
+          h1: {
+            color: 'red'
+          }
+        }
+      },
+      text:`
+      description of the component
+    
+      ~~~js
+      <Button>Click here</Button>
+      ~~~
+      `
+    })(() => (
+    <Button bg="palegoldenrod">Hello World</Button>
+  )))
+  .addWithJSX('with background green', () => (
+    <Button bg="green">Hello World</Button>
+  ));
 
